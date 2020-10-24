4f5f44c638188c39c97a9521ff24785d775c7037
GitKilla <robertnclark1@gmail.com>
2020-10-24 01:09:21 -0400

    testing https redirect

--- server/server.js
+++ server/server.js
@@ -13,3 +13,13 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
    console.log('Server is up!');
 });
+
+function requireHTTPS(req, res, next) {
+   // The 'x-forwarded-proto' check is for Heroku
+   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+     return res.redirect('https://' + req.get('host') + req.url);
+   }
+   next();
+ }
+
+ app.use(requireHTTPS);
