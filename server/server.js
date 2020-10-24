f7e896e2f310cdba9a59dc1e419e67d71deced9e
GitKilla <robertnclark1@gmail.com>
2020-10-24 01:26:26 -0400

    testing https redirect

--- server/server.js
+++ server/server.js
@@ -7,19 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
+   res.redirect('https://' + req.headers.host + req.url);
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
    console.log('Server is up!');
 });
-
-function requireHTTPS(req, res, next) {
-   // The 'x-forwarded-proto' check is for Heroku
-   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-     return res.redirect('https://' + req.get('host') + req.url);
-   }
-   next();
- }
-
- app.use(requireHTTPS);
