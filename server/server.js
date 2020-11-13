80e061af9db6f628a6de907900be5d6ad5419b62
GitKilla <robertnclark1@gmail.com>
2020-11-13 15:04:23 -0500

    add express

--- server/server.js
+++ server/server.js
@@ -6,23 +6,26 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-   console.log("=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]");
-   console.log("Req secure: "+String(req.secure))
-   console.log("Req fwd proto: "+String(req.get('x-forwarded-proto')))
-   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-      // console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-      // res.redirect('https://' + req.headers.host + req.url);
-      console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-      res.sendFile(path.join(publicPath, 'index.html'));
-   } else {
-      console.log("=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>");
-      console.log("public path: "+publicPath);
-      console.log("=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>");
-      res.sendFile(path.join(publicPath, 'index.html'));
-   }
-});
+// app.get('*', (req, res) => {
+//    console.log("=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]=-]");
+//    console.log("Req secure: "+String(req.secure))
+//    console.log("Req fwd proto: "+String(req.get('x-forwarded-proto')))
+//    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+//       // console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+//       // res.redirect('https://' + req.headers.host + req.url);
+//       console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+//       res.sendFile(path.join(publicPath, 'index.html'));
+//    } else {
+//       console.log("=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>");
+//       console.log("public path: "+publicPath);
+//       console.log("=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>");
+//       res.sendFile(path.join(publicPath, 'index.html'));
+//    }
+// });
 
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 
 app.listen(port, () => {
