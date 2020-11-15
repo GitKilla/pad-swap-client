cc629fe4d3393f55d2b79af8a9f3591e9abe3cb2
GitKilla <robertnclark1@gmail.com>
2020-11-15 18:58:37 -0500

    bug fix v3

--- src/components/App.js
+++ src/components/App.js
@@ -233,7 +233,7 @@ class App extends Component {
 
       var askImages = []
       for(var j = 0; j < offer[1].length; j++) {
-        var res = await fetch('https://axieinfinity.com/api/v2/axies/'+String(offer[0][j]));
+        var res = await fetch('https://axieinfinity.com/api/v2/axies/'+String(offer[1][j]));
         res = await res.json();
         askImages.push(res.image)
       }
@@ -261,7 +261,7 @@ class App extends Component {
       var offer = await this.getOfferData(offerId)
       var offerImages = []
       
-      for(var j = 0; j < offer[1].length; j++) {
+      for(var j = 0; j < offer[0].length; j++) {
         var res = await fetch('https://axieinfinity.com/api/v2/axies/'+String(offer[0][j]));
         res = await res.json();
         offerImages.push(res.image)
@@ -269,7 +269,7 @@ class App extends Component {
 
       var askImages = []
       for(var j = 0; j < offer[1].length; j++) {
-        var res = await fetch('https://axieinfinity.com/api/v2/axies/'+String(offer[0][j]));
+        var res = await fetch('https://axieinfinity.com/api/v2/axies/'+String(offer[1][j]));
         res = await res.json();
         askImages.push(res.image)
       }
